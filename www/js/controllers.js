483335a99395d169da48ee78b442e2076f8f8906
Denni Adam <dennila2@ya.ru>
2015-09-01 12:40:55 +0300

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,157 @@
+angular.module('starter.controllers', [])
+
+  .controller('AppCtrl', function ($rootScope, $scope, $ionicModal, $timeout, Persistence) {
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+
+    // Playlist dialog part
+
+    // Form data for the add playlist modal
+    $scope.playlistData = {};
+
+    // Create the add playlist modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/add-playlist.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.addDialog = modal;
+    });
+
+    // Triggered in the add playlist modal to close it
+    $scope.closeAddDialog = function () {
+      $scope.addDialog.hide();
+    };
+
+    // Open the add playlist modal
+    $scope.showAddDialog = function () {
+      $scope.addDialog.show();
+    };
+
+    // Perform the add playlist action when the user submits the add playlist form
+    $scope.doAddDialog = function () {
+
+      var playlist = new Persistence.Entities.Playlist({title: $scope.playlistData.title});
+      console.log('title: ' + $scope.playlistData.title);
+      Persistence.add(playlist);
+      $scope.$broadcast('playlistAdded', playlist);
+
+      $scope.playlistData.title = '';
+      $scope.closeAddDialog();
+    };
+  })
+
+  .controller('PlaylistsCtrl', function ($scope, $ionicModal, Persistence) {
+    $scope.ctrls = {delete: false, reorder: false};
+    $scope.playlists = [];
+
+    var getPlaylists = function () {
+      Persistence.getAllPlaylists().then(function (response) {
+        $scope.playlists.length = 0;
+        for (var i = 0; i < response.length; i++) {
+          $scope.playlists.push(response[i]);
+        }
+      });
+    };
+
+    getPlaylists();
+
+    $scope.movePlayList = function (playList, fromI, toI) {
+
+      if (fromI < toI) {
+        for (var i = fromI; i <= toI; i++) {
+          if (i === fromI) {
+            $scope.playlists[i].order = toI;
+          } else {
+            $scope.playlists[i].order = (i - 1);
+          }
+        }
+      } else if (fromI > toI) {
+        for (i = toI; i <= fromI; i++) {
+          if (i === fromI) {
+            $scope.playlists[i].order = toI;
+          } else {
+            $scope.playlists[i].order = (i + 1);
+          }
+        }
+      }
+
+      Persistence.flush()
+        .then(function (data) {
+          getPlaylists();
+        })
+      ;
+    };
+
+    /** >> edit playlist >> */
+      // Create the edit playlist modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/edit-playlist.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.editDialog = modal;
+    });
+
+    // Triggered in the edit playlist modal to close it
+    $scope.closeEditDialog = function () {
+      $scope.editDialog.hide();
+    };
+
+    // Perform the edit playlist action when the user submits the edit playlist form
+    $scope.doEditDialog = function () {
+
+      var playlist = new Persistence.Entities.Playlist({title: $scope.playlistData.title});
+      Persistence.flush();
+      $scope.$broadcast('playlistEdited', playlist);
+
+      $scope.closeEditDialog();
+    };
+
+    // Open the edit playlist modal
+    $scope.editPlayList = function (playList) {
+      $scope.playlist = playList;
+      $scope.editDialog.show();
+    };
+    /** << edit playlist << */
+
+    $scope.deletePlayList = function (playlist) {
+      Persistence.remove(playlist);
+      getPlaylists();
+    };
+
+    $scope.$on('playlistAdded', function (event, playlist) {
+      getPlaylists();
+    });
+    $scope.$on('playlistEdited', function (event, playlist) {
+      getPlaylists();
+    });
+  })
+
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {
+
+  });
---
+++ www/js/services.js
@@ -0,0 +1,60 @@
+angular.module('starter.services', [])
+
+  .factory('Persistence', function ($q) {
+    //persistence.store.memory.config(persistence);  
+
+    persistence.store.cordovasql.config(persistence, 'app_db', '0.0.1', 'Database description', 5 * 1024 * 1024, 0);
+
+    var entities = {};
+
+    entities.Playlist = persistence.define('Playlist', {
+      title: 'TEXT',
+      order: 'INT'
+    });
+
+    persistence.debug = true;
+    persistence.schemaSync();
+
+    return {
+      Entities: entities,
+
+      add: function (playlist) {
+        entities.Playlist.all().count(null, function (cnt) {
+          console.log('pl count = ' + cnt);
+          playlist.order = cnt;
+          persistence.add(playlist);
+          persistence.flush();
+        });
+      },
+      remove: function (playlist) {
+        persistence.remove(playlist).flush();
+      },
+      getPlaylists: function (from, to) {
+        var defer = $q.defer();
+
+        entities.Playlist.all().list(null, function (playlists) {
+          defer.resolve(playlists);
+        });
+
+        return defer.promise;
+      },
+      getAllPlaylists: function () {
+        var defer = $q.defer();
+
+        entities.Playlist.all().order("order", true).list(null, function (playlists) {
+          defer.resolve(playlists);
+        });
+
+        return defer.promise;
+      },
+      flush: function () {
+        var deferred = $q.defer();
+
+        persistence.flush(function () {
+            deferred.resolve({state: 'ok'});
+          });
+
+        return deferred.promise;
+      }
+    };
+  });
